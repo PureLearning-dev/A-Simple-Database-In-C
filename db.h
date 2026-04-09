65a15e99d0bacf006ebeeb27a5fd79855a2ffdf9
PureLearning-dev <liu.jie.coder@gmail.com>
2026-04-09 22:01:46 +0800

    feat: 项目重构

---
+++ db.h
@@ -0,0 +1,90 @@
+#ifndef DB_H
+#define DB_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define TABLE_MAX_PAGES 250
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+    Row row_to_insert;
+} Statement;
+
+typedef struct {
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+typedef enum {
+    EXECUTE_TABLE_FULL,
+    EXECUTE_SUCCESS
+} ExecuteResult;
+
+// 常量声明
+extern const uint32_t ID_SIZE;
+extern const uint32_t USERNAME_SIZE;
+extern const uint32_t EMAIL_SIZE;
+extern const uint32_t ID_OFFSET;
+extern const uint32_t USERNAME_OFFSET;
+extern const uint32_t EMAIL_OFFSET;
+extern const uint32_t PAGE_SIZE;
+extern const uint32_t ROW_SIZE;
+extern const uint32_t ROWS_PER_PAGE;
+extern const uint32_t TABLE_MAX_ROWS;
+
+// 函数声明
+InputBuffer *new_input_buffer();
+void print_prompt();
+void read_input(InputBuffer *input_buffer);
+void close_input_buffer(InputBuffer *input_buffer);
+
+void serialize_row(Row* source, void* destination);
+void deserialize_row(void* source, Row* destination);
+void* row_slot(Table* table, uint32_t row_num);
+void print_row(Row* row);
+
+Table* new_table();
+void free_table(Table* table);
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table);
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+ExecuteResult execute_statement(Statement* statement, Table *table);
+
+#endif
